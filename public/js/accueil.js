f81c6f1a1a62a84eb224e5092ca5760fc387e74b
Mickaël <mickael.durand@outlook.com>
2024-03-15 16:01:15 +0100

    Commentaire sur le JS

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -1,14 +1,24 @@
+/**
+ * Affiche les informations sur la page d'accueil
+ * @param {array} tabDataApropos - Tableau des informations à afficher
+ */
 function afficheApropos(tabDataApropos) {
+    //On parcours chaque élément
     tabDataApropos.forEach(data => {
+        //On récupère la div qui va contenir le bloc courant
         let divAffichage = document.getElementById(data.id);
-        let templateHtml = ``
+        //On initialise le template HTML 
+        let templateHtml = ``;
 
+        //En fonction de l'id du bloc, on choisit le bon template
         switch (data.id) {
+            //Bloc à propos
             case "aPropos":
                 templateHtml += `<h2>${data.titre}</h2>
                  <p><em>${data.metier}</em></p> 
                  <p><em>${data.lieu}</em></p>`;
                 break;
+            //Bloc des compétences
             case "softSkills":
                 templateHtml += `<h2>${data.titre}</h2>
                                 <div class="flex gap">`;
@@ -17,9 +27,11 @@ function afficheApropos(tabDataApropos) {
                 });
                 templateHtml += `</div>`;
                 break;
+            //Bloc de descrption
             case "description":
                 templateHtml += `<p>${data.texte}</p>`;
                 break;
+            //Bloc des formations
             case "formation":
                 templateHtml += `<h2>${data.titre}</h2>`;
                 data.etablissements.forEach(etablissement => {
@@ -31,6 +43,7 @@ function afficheApropos(tabDataApropos) {
                     templateHtml += `</ul>`;
                 });
                 break;
+            //Bloc des langage de développement
             case "devLanguage":
                 templateHtml += `<h2>${data.titre}</h2>
                                 <div class="flex gap">`;
@@ -39,6 +52,7 @@ function afficheApropos(tabDataApropos) {
                 });
                 templateHtml += `</div>`;
                 break;
+            //Bloc des outils de développement
             case "devTools":
                 templateHtml += `<h2>${data.titre}</h2>
                                 <div class="flex gap">`;
@@ -51,40 +65,61 @@ function afficheApropos(tabDataApropos) {
             default:
                 break;
         }
+        //On affiche le template dans notre div
         divAffichage.innerHTML = templateHtml;
     });
 }
 
+/**
+ * Affiche le détail de la carrière
+ * @param {array} tabDataCarriere - Tableau des informations de carrière à afficher
+ */
 function afficheCarriere(tabDataCarriere) {
+    //On parcourt l'ensemble des emplois
     tabDataCarriere.forEach(data => {
+        //On récupère la div qui va contenir le bloc courant
         let divAffichage = document.getElementsByClassName(`carriere-${data.id}`)[0];
+        //On initialise le template HTML 
         let templateHtml = ``;
 
+        //Si le métier n'est pas vide, on l'affiche
         if (data.metier != ``) {
             templateHtml += `<h2 class="flex justify-between"><div>${data.metier} - ${data.employeur}</div> <a href="${data.link}" title="Accès au site de ${data.employeur}" target="_blank"><i class="fa-solid fa-globe"></i></a></h2> 
             <h3>${data.lieu} - ${data.dates}</h3> 
             <p>${data.description}</p>`;
         }
         else {
+            //Sinon on affiche Job Undefined
             templateHtml += `Job Undefined`
         }
+        //On affiche le template dans notre div
         divAffichage.innerHTML = templateHtml;
     });
 }
 
+/**
+ * Affiche les différents projets
+ * @param {array} tabDataProjets - Tableau des projets avec leurs informations
+ */
 function afficheProjets(tabDataProjets) {
+    //On parcourt l'ensemble des projets
     tabDataProjets.forEach(data => {
+        //On récupère la div qui va contenir le bloc courant
         let divAffichage = document.getElementsByClassName(`projet-${data.id}`)[0];
+        //On initialise le template HTML 
         let templateHtml = ``;
 
+        //Si le libellé du projet n'est pas vide, on l'affiche
         if (data.libelle != ``) {
             divAffichage.style = `background-image: url("./public/img/images/${data.background}");color:${data.color};`;
             templateHtml += `${data.competence}<a class="picto picto-36 rounded picto-center fixed-bottom-right" href="./projet.html?projet-id=${data.id}" title="Accès au projet">
                 <i class="fa-solid fa-plus"></i></a>`;
         }
         else {
+            //Sinon on affiche New Project Soon
             templateHtml += `New project soon...`
         }
+        //On affiche le template dans notre div
         divAffichage.innerHTML = templateHtml;
     });
 }
@@ -93,6 +128,7 @@ function afficheProjets(tabDataProjets) {
 fetch(`./public/json/accueil.json`).then(res => {
     return res.json();
 }).then(rep => {
+        //On appelle nos fonctions d'affichage
         afficheApropos(rep.apropos);
         afficheCarriere(rep.carriere);
         afficheProjets(rep.projets);
